ed67862297daea392e10773cb36a4bcd45e34311
feldman <feldman4@mit.edu>
2016-12-12 07:49:17 -0500

    ok

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,13 @@ module ComponentTemplate exposing (Model, Msg(..), init, update, view)
 import Html exposing (text, div, program, ul)
 import Html.Attributes exposing (id, class, style)
 import Html.Events exposing (onClick)
+import Json.Decode as JD
+import Json.Encode as JE
+import Random.Pcg as Random
+import Time
+import Debug
+import Task
+import WebSocket
 
 
 main : Program Never Model Msg
@@ -20,30 +27,74 @@ main =
 
 
 type alias Model =
-    { challenge : ( String, String )
+    { challenge : List String
     , input : List String
-    , score : Float
+    , score : List ( Float, Float )
     , choices : ( List String, List String )
     , appState : AppState
+    , seed : Random.Seed
     }
 
 
 init : ( Model, Cmd Msg )
 init =
     let
-        adjectives =
-            [ "unctuous", "unwashed", "gutless", "defrocked", "preposterous" ]
-
-        nouns =
-            [ "weakling", "backstabber", "wimp", "criminal", "squatter" ]
+        a =
+            Debug.log "word counts" ( List.length adjectives, List.length nouns )
     in
-        { challenge = ( "Rancid", "Felon" )
+        { challenge = [ "Rancid", "Felon" ]
         , input = []
-        , choices = ( adjectives, nouns )
-        , score = 0
+        , choices =
+            ( [], [] )
+            -- get filled in randomly with time
+        , score = []
         , appState = Splash
+        , seed =
+            Random.initialSeed 0
+            -- replaced by time
         }
-            ! []
+            ! [ Task.perform NewTime Time.now ]
+
+
+initVocab : Int -> List String -> Random.Generator (List String)
+initVocab n vocab =
+    let
+        vocabSize =
+            List.length vocab
+    in
+        Random.list vocabSize (Random.float 0 1)
+            |> Random.map argSort
+            |> Random.map (indexList vocab)
+            |> Random.map (List.take n)
+
+
+initChallenge : ( Random.Generator String, Random.Generator String )
+initChallenge =
+    ( Random.sample adjectives |> Random.map (Maybe.withDefault "random-adjective")
+    , Random.sample nouns |> Random.map (Maybe.withDefault "random-noun")
+    )
+
+
+indexList : List a -> List Int -> List a
+indexList data index =
+    let
+        f i =
+            case List.drop i data |> List.head of
+                Just x ->
+                    [ x ]
+
+                Nothing ->
+                    []
+    in
+        index |> List.map f |> List.concat
+
+
+argSort : List comparable -> List Int
+argSort input =
+    input
+        |> List.indexedMap (\a b -> ( b, a ))
+        |> List.sort
+        |> List.map Tuple.second
 
 
 type AppState
@@ -59,7 +110,8 @@ type AppState
 type Msg
     = Click String
     | ToState AppState
-    | NoOp
+    | NewTime Time.Time
+    | Score String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -67,23 +119,150 @@ update msg model =
     case msg of
         Click choice ->
             let
-                appState =
+                newInput =
+                    model.input ++ [ choice ]
+
+                ( appState, newCmd ) =
                     if ((List.length model.input) % 2) == 1 then
-                        Map
+                        ( Map, (requestScore model.challenge newInput model.choices) )
                     else
-                        model.appState
+                        model.appState ! []
             in
                 { model
-                    | input = model.input ++ [ choice ]
+                    | input = newInput
                     , appState = appState
+                }
+                    ! [ newCmd ]
+
+        ToState appState ->
+            case appState of
+                Prompt ->
+                    if (List.length model.input) == 6 then
+                        init
+                    else
+                        { model | appState = appState } ! []
+
+                _ ->
+                    { model | appState = appState } ! []
+
+        NewTime time ->
+            let
+                seed0 =
+                    Random.initialSeed (round time)
+
+                ( gA, gB ) =
+                    initChallenge
+
+                ( wordA, seed2 ) =
+                    seed0 |> Random.step gA
+
+                ( wordB, seed3 ) =
+                    seed2 |> Random.step gB
+
+                newModel =
+                    { model
+                        | seed = seed3
+                        , challenge = [ wordA, wordB ]
+                    }
+
+                ( adj, nou, finalSeed ) =
+                    getAdjNoun newModel
+            in
+                { newModel | choices = ( adj, nou ), seed = finalSeed }
+                    ! []
+
+        Score s ->
+            let
+                a =
+                    Debug.log "incoming" s
+
+                ( scoreA, scoreB ) =
+                    decodeScores s
+
+                ( adj, nou, seed ) =
+                    getAdjNoun model
+            in
+                { model
+                    | score = model.score ++ [ ( scoreA, scoreB ) ]
+                    , choices = ( adj, nou )
+                    , seed = seed
                 }
                     ! []
 
-        ToState appState ->
-            { model | appState = appState } ! []
 
-        NoOp ->
-            model ! []
+getAdjNoun : Model -> ( List String, List String, Random.Seed )
+getAdjNoun model =
+    let
+        f x =
+            not (List.member x (model.input ++ model.challenge))
+
+        adjectivesFilt =
+            List.filter f adjectives
+
+        nounsFilt =
+            List.filter f nouns
+
+        ( adj, seed1 ) =
+            model.seed |> Random.step (initVocab 5 adjectivesFilt)
+
+        ( nou, seed2 ) =
+            seed1 |> Random.step (initVocab 5 nounsFilt)
+    in
+        ( adj, nou, seed2 )
+
+
+
+-- REQUEST
+-- we are going to ask the server for a score based on the user's choice
+
+
+type alias Choices =
+    ( List String, List String )
+
+
+decodeScores : String -> ( Float, Float )
+decodeScores json =
+    let
+        decoder x =
+            case x of
+                Ok y ->
+                    y
+
+                Err z ->
+                    let
+                        a =
+                            Debug.log "err" z
+                    in
+                        -1
+
+        choiceA =
+            JD.decodeString (JD.field "scoreA" JD.float) json |> decoder
+
+        choiceB =
+            JD.decodeString (JD.field "scoreB" JD.float) json |> decoder
+    in
+        ( choiceA, choiceB )
+
+
+requestScore : List String -> List String -> Choices -> Cmd Msg
+requestScore challenge input choices =
+    let
+        listString x =
+            List.map JE.string x |> JE.list
+
+        request =
+            JE.object
+                [ ( "challenge", listString challenge )
+                , ( "input", listString input )
+                , ( "choicesA", listString (Tuple.first choices) )
+                , ( "choicesB", listString (Tuple.second choices) )
+                ]
+                |> JE.encode 4
+
+        a =
+            Debug.log "sent" request
+    in
+        WebSocket.send "ws://localhost:5000/score" request
 
 
 
@@ -135,7 +314,7 @@ viewSplash model =
             "Dave is an impostor. He's really a"
 
         impostorId =
-            (Tuple.first model.challenge) ++ " " ++ (Tuple.second model.challenge)
+            String.join " " model.challenge
 
         impostorDiv =
             div [ id "intro-dave", (style [ ( "color", "green" ) ]) ] [ text impostorId ]
@@ -178,7 +357,7 @@ viewMap model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    [] |> Sub.batch
+    [ WebSocket.listen "ws://localhost:5000/score" Score ] |> Sub.batch
 
 
 
@@ -190,3 +369,279 @@ subscriptions model =
 --     , Sub.map SpeechMsg (Speech.subscriptions model.speechModel)
 --     ]
 --         |> Sub.batch
+-- DATA
+
+
+adjectives : List String
+adjectives =
+    [ "apprentice"
+    , "craven"
+    , "defenceless"
+    , "orthodox"
+    , "alcoholic"
+    , "antediluvian"
+    , "armchair"
+    , "asthmatic"
+    , "atrophied"
+    , "avaricious"
+    , "backwater"
+    , "backwoods"
+    , "bankrupt"
+    , "bearded"
+    , "bellicose"
+    , "belligerent"
+    , "bigoted"
+    , "botched"
+    , "breastfed"
+    , "bucktoothed"
+    , "cantankerous"
+    , "careless"
+    , "celebrated"
+    , "clamorous"
+    , "closet"
+    , "clumsy"
+    , "common"
+    , "constipated"
+    , "crass"
+    , "daft"
+    , "deceased"
+    , "defrocked"
+    , "deranged"
+    , "despicable"
+    , "dimwitted"
+    , "dirty"
+    , "diseased"
+    , "disgraced"
+    , "disguised"
+    , "dishonest"
+    , "disingenuous"
+    , "disorganized"
+    , "drugstore"
+    , "embarrassing"
+    , "floundering"
+    , "forsaken"
+    , "foul"
+    , "fraudulent"
+    , "garish"
+    , "gilded"
+    , "gutless"
+    , "hairless"
+    , "humorless"
+    , "ignorant"
+    , "impotent"
+    , "impoverished"
+    , "incontinent"
+    , "indiscreet"
+    , "ineffectual"
+    , "insignificant"
+    , "insincere"
+    , "insufferable"
+    , "intellectual"
+    , "inveterate"
+    , "irrelevant"
+    , "jackbooted"
+    , "jaded"
+    , "jilted"
+    , "kowtowing"
+    , "licentious"
+    , "listless"
+    , "litigious"
+    , "loathsome"
+    , "lonely"
+    , "luckless"
+    , "malignant"
+    , "mean"
+    , "meddlesome"
+    , "medicated"
+    , "melancholy"
+    , "misinformed"
+    , "mistaken"
+    , "monied"
+    , "morose"
+    , "nauseating"
+    , "obsequious"
+    , "odious"
+    , "officious"
+    , "overfunded"
+    , "overpaid"
+    , "pallid"
+    , "poisonous"
+    , "political"
+    , "pompous"
+    , "powdered"
+    , "precocious"
+    , "predatory"
+    , "prejudiced"
+    , "preposterous"
+    , "reckless"
+    , "renegade"
+    , "rented"
+    , "retired"
+    , "scheming"
+    , "simpering"
+    , "sinister"
+    , "sniveling"
+    , "spineless"
+    , "subservient"
+    , "sweaty"
+    , "synthetic"
+    , "talentless"
+    , "tendentious"
+    , "treacherous"
+    , "tufted"
+    , "unctuous"
+    , "underhanded"
+    , "underqualified"
+    , "unelected"
+    , "unemployed"
+    , "unprincipled"
+    , "unregenerate"
+    , "unsuccessful"
+    , "unwashed"
+    , "unwitting"
+    , "variegated"
+    , "vindictive"
+    , "worthless"
+    , "wretched"
+    ]
+
+
+nouns : List String
+nouns =
+    [ "carrion"
+    , "fop"
+    , "fakir"
+    , "halfwit"
+    , "highwayman"
+    , "judas"
+    , "marxist"
+    , "monk"
+    , "moocher"
+    , "mystic"
+    , "poltergeist"
+    , "regurgitator"
+    , "trotskyite"
+    , "undertaker"
+    , "yahoo"
+    , "abuser"
+    , "academic"
+    , "accomplice"
+    , "amateur"
+    , "apostate"
+    , "aristocrat"
+    , "arsonist"
+    , "assassin"
+    , "atheist"
+    , "backstabber"
+    , "bastard"
+    , "betrayer"
+    , "bigmouth"
+    , "boob"
+    , "caveman"
+    , "centipede"
+    , "cheater"
+    , "clerk"
+    , "collaborator"
+    , "communist"
+    , "concubine"
+    , "coward"
+    , "crackpot"
+    , "creature"
+    , "cretin"
+    , "criminal"
+    , "cuckold"
+    , "cultist"
+    , "demagogue"
+    , "dignitary"
+    , "drunk"
+    , "dwarf"
+    , "enigma"
+    , "eunuch"
+    , "executioner"
+    , "experiment"
+    , "failure"
+    , "fanatic"
+    , "fascist"
+    , "fetishist"
+    , "filth"
+    , "fool"
+    , "functionary"
+    , "garbageman"
+    , "gelding"
+    , "goon"
+    , "gravedigger"
+    , "hack"
+    , "hitman"
+    , "huckster"
+    , "hypocrite"
+    , "idealist"
+    , "individualist"
+    , "insect"
+    , "invertebrate"
+    , "jingoist"
+    , "kidnapper"
+    , "latecomer"
+    , "lawbreaker"
+    , "liar"
+    , "looter"
+    , "loser"
+    , "lunatic"
+    , "masturbator"
+    , "mercenary"
+    , "miscreant"
+    , "moron"
+    , "mortician"
+    , "murderer"
+    , "narcissist"
+    , "neophyte"
+    , "nincompoop"
+    , "parasite"
+    , "peasant"
+    , "pederast"
+    , "philanthropist"
+    , "philistine"
+    , "philosopher"
+    , "pilferer"
+    , "pimp"
+    , "plagiarizer"
+    , "poltroon"
+    , "pornographer"
+    , "priest"
+    , "pushover"
+    , "pygmy"
+    , "racist"
+    , "rapist"
+    , "rapist"
+    , "reactionary"
+    , "redneck"
+    , "reptile"
+    , "riffraff"
+    , "scoundrel"
+    , "scum"
+    , "sinner"
+    , "soothsayer"
+    , "sponge"
+    , "spook"
+    , "squatter"
+    , "stenographer"
+    , "swindler"
+    , "swineherd"
+    , "sycophant"
+    , "thug"
+    , "traitor"
+    , "transient"
+    , "trapper"
+    , "trash"
+    , "turd"
+    , "turncoat"
+    , "tyrant"
+    , "upstart"
+    , "vandal"
+    , "villain"
+    , "warlord"
+    , "weakling"
+    , "weasel"
+    , "wimp"
+    , "witchdoctor"
+    , "zealot"
+    ]
